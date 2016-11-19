9bd30f855508779d048689e473f469c80254edd5
mrto11235 <martin.plaznik@gmail.com>
2016-11-19 23:15:56 +0100

    angular controllers and app added

--- avocadoApp/www/js/controllers.js
+++ avocadoApp/www/js/controllers.js
@@ -1,28 +1,34 @@
-angular.module('starter.controllers', [])
+app.controller('a-controller', function($scope, $timeout, $location, $interval) {
+  var promise;
+  $scope.count = 9;
+  $scope.subCount = function(a) {
+    $scope.count = a + $scope.count;
+  }
+  $scope.gameStart = function() {
+    $scope.labela = $location;
+    $location.path('/playground');
+    $scope.count = 30;
+    $scope.start();
+    $timeout(function() {
+      $location.path('/home');
+    }, 30000);
+  }
+  /*$interval(function() {
+    $scope.count--;
+  }, 1000);*/
+  $scope.start = function() {
+    // stops any running interval to avoid two intervals running at the same time
+    $scope.stop();
 
-.controller('DashCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+    // store the interval promise
+    promise = $interval(substractCount, 1000);
   };
-})
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
+  // stops the interval
+  $scope.stop = function() {
+    $interval.cancel(promise);
   };
+  $scope.substractCount = function() {
+    $scope.count--;
+  }
 });
